6b636bbe08234de82ba849233cd9275241361682
Malik Dennis <140343286+MDK35@users.noreply.github.com>
2023-11-01 21:35:26 -0700

    Add files via upload

---
+++ Crowdfunding_etl_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE contacts(
+	contact_id INT PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR (50) NOT Null
+);
+
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM campaign;
+SELECT * FROM subcategory;
